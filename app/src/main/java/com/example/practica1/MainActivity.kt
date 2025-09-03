30c2c5055c6042ed9c559db8646df4dd566095ff
carmenbrachoc5 <carmen.brachoc529@gmail.com>
2025-09-03 11:55:17 -0600

    Nombre con color y número de control centrados

--- app/src/main/java/com/example/practica1/MainActivity.kt
+++ app/src/main/java/com/example/practica1/MainActivity.kt
@@ -31,6 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+
                     GreetingText(
                         "Carmen Bracho",
                         "221000121",
